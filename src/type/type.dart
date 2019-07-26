73828c052b8a8c76f5c54df8f3bc95b10bfe8ee5
Aditya Ladwa <ladwa.aditya@gmail.com>
2019-07-26 20:18:44 +0530

    Feat: Numbers

---
+++ src/type/type.dart
@@ -0,0 +1,4 @@
+export 'type.dart';
+
+var two = 2;
+var numDouble = 1.1;
---
+++ test/type/type_test.dart
@@ -0,0 +1,12 @@
+import 'dart:math';
+
+import 'package:test/test.dart';
+
+import '../../src/type/type.dart';
+
+main() {
+  test("Test numbers", () {
+    assert(two == 2);
+    assert(numDouble == 1.1);
+  });
+}
