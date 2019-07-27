0c2019af651b6737dd0ef99289e36e415ccaad66
Aditya Ladwa <ladwa.aditya@gmail.com>
2019-07-27 19:49:05 +0530

    Feat: Sets and Maps

--- src/type/type.dart
+++ src/type/type.dart
@@ -3,5 +3,13 @@ export 'type.dart';
 var two = 2;
 var numDouble = 1.1;
 var hello = "hello";
-var helloSuper = """hello
+const helloSuper = """hello
 i am super""";
+
+const yes = true;
+const no = false;
+
+var myList = [1, 2, 3];
+var myConstList = const [1, 2];
+var mySet = {1, 2};
+const myMap = {1: "one"};
--- test/type/type_test.dart
+++ test/type/type_test.dart
@@ -16,4 +16,28 @@ i am super""";
     expect("hello", hello);
     expect(testHelloSuper, helloSuper);
   });
+
+  test("True Booleans", () {
+    expect(true, yes);
+    expect(false, no);
+  });
+
+  test("Test Lists", () {
+    expect(myList, [1, 2, 3]);
+    expect(myList[1], 2);
+    expect(myList.length, 3);
+    myList.add(4);
+    expect(myList.length, 4);
+    expect(myList[3], 4);
+    expect([0, 1, 2, 3, 4], [0, ...myList]);
+    expect(myConstList, [1, 2]);
+  });
+
+  test("Test Sets", () {
+    expect({1, 2}, mySet);
+  });
+
+  test("Test Maps", () {
+    expect({1: "one"}, myMap);
+  });
 }
